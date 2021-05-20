3e85c4ba03137fe6cead9f68df638293f3f71135
Zecuel <lauri-mikael.pakkanen@harriot.fi>
2021-05-20 15:23:30 +0300

    Re-commit of emailing capability, with private variables removed from VC

--- src/hooks/useContactForm.tsx
+++ src/hooks/useContactForm.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 
+import emailjs from 'emailjs-com';
+
 import IContactFormState from '../interfaces/IContactFormState';
 
 const useContactForm = (): IContactFormState => {
 
 	const [senderEmail, setSenderEmail] = useState('');
+	const [senderEmailError, setSenderEmailError] = useState(null);
+
 	const [senderName, setSenderName] = useState('');
+	const [senderNameError, setSenderNameError] = useState(null);
+
 	const [messageContent, setMessageContent] = useState('');
+	const [messageContentError, setMessageContentError] = useState(null);
 
 	const onSenderEmailChange = (e: React.FormEvent<HTMLInputElement>) => {
 		setSenderEmail(e.currentTarget.value);
@@ -22,9 +29,24 @@ const useContactForm = (): IContactFormState => {
 
 	const onFormSubmit = (e: React.FormEvent<HTMLInputElement>) => {
 
+		clearFormErrors();
+
+		// TODO: Check validity of inputs
+		// TODO: For security, move email variables to .env
+
 		e.preventDefault();
 
-		const emailTemplateID = '';
+		// TODO: Separate logic to controller
+
+		const emailUserID = process.env.REACT_APP_EMAILJS_USER_ID;
+
+		const emailProviderID = process.env.REACT_APP_EMAILJS_PROVIDER;
+		const emailTemplateID = process.env.REACT_APP_EMAILJS_TEMPLATE;
+
+		if (!(emailUserID && emailProviderID && emailTemplateID)) {
+			// TODO: Handle error
+			return;
+		}
 
 		const messageVariables = {
 			sender_email: senderEmail,
@@ -32,15 +54,35 @@ const useContactForm = (): IContactFormState => {
 			message: messageContent
 		};
 
-		// TODO: implement form submission
+		emailjs.init(emailUserID);
+
+		emailjs.send(emailProviderID, emailTemplateID, messageVariables)
+			.then(() => {
+				// TODO: Handle successful submit
+				console.debug('email sent');
+			})
+			.catch(() => {
+				// TODO: Handle error
+				console.debug('failed to send email');
+			});
+
+	};
+
+	const clearFormErrors = () => {
+		setSenderEmailError(null);
+		setSenderNameError(null);
+		setMessageContentError(null);
 	};
 
 	return {
 		sender: {
 			email: senderEmail,
-			name: senderName
+			emailError: senderEmailError,
+			name: senderName,
+			nameError: senderNameError
 		},
 		messageContent: messageContent,
+		messageContentError: messageContentError,
 		methods: {
 			onSenderEmailChange,
 			onSenderNameChange,
--- src/interfaces/IContactFormState.tsx
+++ src/interfaces/IContactFormState.tsx
@@ -1,11 +1,14 @@
 interface ISender {
 	email: string,
-	name: string
+	emailError: null | string,
+	name: string,
+	nameError: null | string
 }
 
 interface IContactFormState {
 	sender: ISender,
 	messageContent: string,
+	messageContentError: null | string,
 	methods: {
 		[key: string]: (e: any) => void
 	}
