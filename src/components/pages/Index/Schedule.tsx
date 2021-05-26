791cab227e78d1378dc73c161292dd877b7348fa
Zecuel <lauri-mikael.pakkanen@harriot.fi>
2021-05-26 11:45:02 +0300

    Added functionality for reading schedule data from csv file

--- src/components/pages/Index/Schedule.tsx
+++ src/components/pages/Index/Schedule.tsx
@@ -1,14 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import papa from 'papaparse';
 
 import { TableColumn, TableRow, TableRowSeparator } from './ScheduleComponents';
 
 import styles from './Schedule.module.css';
+
+interface IScheduleDataRow {
+	startCountry: string,
+	endCountry: string,
+	startTime: string,
+	endTime: string
+}
+
+interface IScheduleData {
+	startCountries: string[],
+	endCountries: string[],
+	startTimes: string[],
+	endTimes: string[]
+}
+
+interface IGetDataRows {
+	data: null | IScheduleData
+	column: IScheduleFields,
+}
+
+type IScheduleFields = 'startCountries' | 'endCountries' | 'startTimes' | 'endTimes';
 
 interface IScheduleTitle {}
 
 interface IScheduleBody {}
 
 interface ISchedule {}
+
+const getScheduleData = async () => {
+
+	const getCSVData = async () => {
+
+		try {
+
+			const fileContents = await fetch('/assets/Schedule.csv');
+
+			if (!fileContents.body) {
+				return false;
+			}
+
+			const reader = fileContents.body.getReader();
+			const readResult = await reader.read();
+
+			const decoder = new TextDecoder('utf-8');
+
+			return decoder.decode(readResult.value);
+
+		} catch (err) {
+			console.error(err);
+			return false;
+		}
+
+	};
+
+	const csvData = await getCSVData();
+
+	if (!csvData) {
+		return false;
+	}
+
+	const parsedData = papa.parse(csvData, { header: true });
+
+	if (parsedData.errors.length !== 0) {
+		console.error(parsedData.errors);
+		return false;
+	}
+
+	if (!parsedData.data) {
+		return false;
+	}
+
+	return parsedData.data;
+
+};
 
 const ScheduleTitle: React.FC<IScheduleTitle> = () => {
 
@@ -20,9 +89,62 @@ const ScheduleTitle: React.FC<IScheduleTitle> = () => {
 
 };
 
+const GetDataRows: React.FC<IGetDataRows> = (props) => {
 
+	if (!(props.data && props.column)) { return <TableRow /> }
+
+	const jsx = props.data[props.column].map(
+		(row: string) => <TableRow key={Math.random()}>{row}</TableRow>
+	);
+
+	return (
+		<>{jsx}</>
+	);
+
+};
 
 const ScheduleBody: React.FC<IScheduleBody> = () => {
+
+	const [scheduleData, setScheduleData] = useState<null | IScheduleData>(null)
+
+	useEffect(() => {
+
+		getScheduleData().then((data) => {
+
+			if (!data) {
+				console.error('No data was found for schedule');
+				return false;
+			}
+
+
+			const startCountries = [];
+			const endCountries = [];
+			const startTimes = [];
+			const endTimes = [];
+
+			for (let _row of data) {
+
+				const row = _row as IScheduleDataRow;
+
+				startCountries.push(row.startCountry);
+				endCountries.push(row.endCountry);
+				startTimes.push(row.startTime);
+				endTimes.push(row.endTime);
+
+			}
+
+			const resultData = {
+				startCountries,
+				endCountries,
+				startTimes,
+				endTimes
+			};
+
+			setScheduleData(resultData);
+
+		});
+
+	}, []);
 
 	return (
 		<div className={styles.ScheduleBody}>
@@ -35,9 +157,7 @@ const ScheduleBody: React.FC<IScheduleBody> = () => {
 
 				<TableRowSeparator />
 
-				<TableRow>
-					Suomi
-				</TableRow>
+				<GetDataRows data={scheduleData} column={'startCountries'} />
 
 			</TableColumn>
 
@@ -49,9 +169,7 @@ const ScheduleBody: React.FC<IScheduleBody> = () => {
 
 				<TableRowSeparator />
 
-				<TableRow>
-					Espanja
-				</TableRow>
+				<GetDataRows data={scheduleData} column={'endCountries'} />
 
 			</TableColumn>
 
@@ -63,9 +181,7 @@ const ScheduleBody: React.FC<IScheduleBody> = () => {
 
 				<TableRowSeparator />
 
-				<TableRow>
-					29.5.2021
-				</TableRow>
+				<GetDataRows data={scheduleData} column={'startTimes'} />
 
 			</TableColumn>
 
@@ -77,9 +193,7 @@ const ScheduleBody: React.FC<IScheduleBody> = () => {
 
 				<TableRowSeparator />
 
-				<TableRow>
-					6.6.2021
-				</TableRow>
+				<GetDataRows data={scheduleData} column={'endTimes'} />
 
 			</TableColumn>
 
