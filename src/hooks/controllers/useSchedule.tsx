321cbd898d8d585211695072677b2803c0b3189d
Zecuel <lauri-mikael.pakkanen@harriot.fi>
2021-07-15 16:02:41 +0300

    Fixed issue with hook

--- src/hooks/controllers/useSchedule.tsx
+++ src/hooks/controllers/useSchedule.tsx
@@ -12,71 +12,71 @@ interface IScheduleDataRow {
 	hasSpaceAvailable: string
 }
 
-const getScheduleData = async (memoryController: IUSeMemoryController) => {
-
-	const getCSVData = async () => {
-
-		try {
-
-			const fileContents = await fetch(`${constants.BASE_URI}/assets/Schedule.csv`);
-
-			if (!fileContents.body) {
-				const error = new CError('No data was found for Schedule', 404, false);
-				memoryController.addMemoryError(error);
-				return;
-			}
-
-			const reader = fileContents.body.getReader();
-			const readResult = await reader.read();
-
-			const decoder = new TextDecoder('utf-8');
-
-			return decoder.decode(readResult.value);
-
-		} catch (err) {
-			memoryController.addMemoryError(new CError(err.message, err.code, false));
-			return;
-		}
-
-	};
-
-	const csvData = await getCSVData();
-
-	if (!csvData) {
-		memoryController.addMemoryError(new CError('No data was found for Schedule', 404, false));
-		return;
-	}
-
-	const parsedData = papa.parse(csvData, { header: true, skipEmptyLines: true });
-
-	if (parsedData.errors.length !== 0) {
-
-		const err = parsedData.errors[0];
-
-		const errMessage = err.message;
-		const errCode = parseInt(err.code);
-
-		memoryController.addMemoryError(new CError(errMessage, errCode,false));
-		return;
-
-
-	}
-
-	if (!parsedData.data) {
-		const error = new CError('No data was found for Schedule', 404,false);
-		memoryController.addMemoryError(error);
-		return;
-	}
-
-	return parsedData.data;
-
-};
-
 const useSchedule = (): IScheduleDataRow[] => {
 
 	const [scheduleData, setScheduleData] = useState<IScheduleDataRow[]>([]);
 
 	const memoryController = useMemoryController();
+
+	const getScheduleData = async (memoryController: IUSeMemoryController) => {
+
+		const getCSVData = async () => {
+
+			try {
+
+				const fileContents = await fetch(`${constants.BASE_URI}/assets/Schedule.csv`);
+
+				if (!fileContents.body) {
+					const error = new CError('No data was found for Schedule', 404, false);
+					memoryController.addMemoryError(error);
+					return;
+				}
+
+				const reader = fileContents.body.getReader();
+				const readResult = await reader.read();
+
+				const decoder = new TextDecoder('utf-8');
+
+				return decoder.decode(readResult.value);
+
+			} catch (err) {
+				memoryController.addMemoryError(new CError(err.message, err.code, false));
+				return;
+			}
+
+		};
+
+		const csvData = await getCSVData();
+
+		if (!csvData) {
+			memoryController.addMemoryError(new CError('No data was found for Schedule', 404, false));
+			return;
+		}
+
+		const parsedData = papa.parse(csvData, { header: true, skipEmptyLines: true });
+
+		if (parsedData.errors.length !== 0) {
+
+			const err = parsedData.errors[0];
+
+			const errMessage = err.message;
+			const errCode = parseInt(err.code);
+
+			memoryController.addMemoryError(new CError(errMessage, errCode,false));
+			return;
+
+
+		}
+
+		if (!parsedData.data) {
+			const error = new CError('No data was found for Schedule', 404,false);
+			memoryController.addMemoryError(error);
+			return;
+		}
+
+		return parsedData.data;
+
+	};
 
 	useEffect(() => {
 
